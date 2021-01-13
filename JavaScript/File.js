5acf4cb20718c3510dec4b532f5c5aba41136a47
rohit001007 <rohit001007@gmail.com>
2021-01-13 20:30:05 +0530

    Updated The Code

--- JavaScript/File.js
+++ JavaScript/File.js
@@ -1,30 +1,23 @@
 $(document).ready(function () {
+$.getJSON("../JSON/Data.json",
+    
+function (data) {
+    var rowData = "";
 
-    $.getJSON("../JSON/Data.json",
-        function (data) {
-            var emp = "";
+        $.each(data, function (key, value) {
 
-            $.each(data, function (key, value) {
+            rowData += "<tr>";
 
+            rowData += "<td>" + value.firstName + "</td>";
+            rowData += '<td>' + value.lastName + '</td>';
+            rowData += '<td>' + value.eMail + '</td>';
+            rowData += '<td>' + value.mobile + '</td>';
+            rowData += '<td>' + value.companyName + '</td>';
 
-                emp += "<tr>";
-                emp += "<td>" +
-                    value.firstName + "</td>";
-                emp += '<td>' +
-                    value.lastName + '</td>';
-                emp += '<td>' +
-                    value.eMail + '</td>';
-                emp += '<td>' +
-                    value.mobile + '</td>';
-                emp += '<td>' +
-                    value.companyName + '</td>';
-
-                emp += '<td><img src="../Images/edit-list-svgrepo-com.svg"></td>';
-                emp += '<td><img src="../Images/delete-contact-svgrepo-com.svg"></td>';
-
-                "</tr>";
-            });
-
-            $('#table').append(emp);
+            rowData += '<td><img src="../Images/edit-list-svgrepo-com.svg"></td>';
+            rowData += '<td><img src="../Images/delete-contact-svgrepo-com.svg"></td>';"</tr>";
         });
+
+        $('#table').append(rowData);
+    });
 }); 
