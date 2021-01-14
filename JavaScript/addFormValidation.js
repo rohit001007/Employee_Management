f7f4005c5d2cfae8c80a32f0f76d4a37365df32c
rohit001007 <rohit001007@gmail.com>
2021-01-14 21:48:26 +0530

    Added : First Get Operation Is Done

--- JavaScript/addFormValidation.js
+++ JavaScript/addFormValidation.js
@@ -1,25 +1,25 @@
 function check() {
 
-    var firstName = document.getElementById('firstName').value;
-    var lastName = document.getElementById('lastName').value;
-    var eMail = document.getElementById('eMail').value;
-    var mobile = document.getElementById('mobile').value;
+    let firstName = document.getElementById('firstName').value;
+    let lastName = document.getElementById('lastName').value;
+    let eMail = document.getElementById('eMail').value;
+    let mobile = document.getElementById('mobile').value;
     
-    var firstNamePattern = RegExp(`^[A-Z]{1}[a-z]{2,}$`);
-    if (firstNamePattern.test(firstName) == false)
-        document.getElementById('firstName_err').innerHTML = "*Enter Last Name Properly";
+    let firstNamePattern = RegExp(`^[A-Z]{1}[a-z]{2,}$`);
+        if (firstNamePattern.test(firstName) == false)
+            document.getElementById('firstName_err').innerHTML = "*Enter First Name Properly";
         
-    var lastNamePattern = RegExp(`^[A-Z]{1}[a-z]{2,}$`);
-    if (lastNamePattern.test(lastName) == false)
-        document.getElementById('lastName_err').innerHTML = "*Enter Last Name Properly";
+    let lastNamePattern = RegExp(`^[A-Z]{1}[a-z]{2,}$`);
+        if (lastNamePattern.test(lastName) == false)
+            document.getElementById('lastName_err').innerHTML = "*Enter Last Name Properly";
     
-    var eMailPattern = RegExp(`^[a-zA-Z0-9]([._+-]{0,1}[a-zA-Z0-9])*[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2,3}){0,1}$`);
-    if (eMailPattern.test(eMail) == false)
-        document.getElementById('eMail_err').innerHTML = "*Enter E-Mail Properly";
+    let eMailPattern = RegExp(`^[a-zA-Z0-9]([._+-]{0,1}[a-zA-Z0-9])*[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2,3}){0,1}$`);
+        if (eMailPattern.test(eMail) == false)
+            document.getElementById('eMail_err').innerHTML = "*Enter E-Mail Properly";
     
-    var mobilePattern = RegExp(`^([+][1-9]{1,3}[- ]?)?[1-9]{10}$`);
-    if (mobilePattern.test(mobile) == false)
-        document.getElementById('mobile_err').innerHTML = "*Enter Mobile-Number Properly";
+    let mobilePattern = RegExp(`^([+][1-9]{1,3}[- ]?)?[1-9]{10}$`);
+        if (mobilePattern.test(mobile) == false)
+            document.getElementById('mobile_err').innerHTML = "*Enter Mobile-Number Properly";
         
     return false;
 }
--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,18 @@
 const express = require('express')
+var cors = require('cors')
 const app = express();
 
 const route = require('../Route/route')
 
 let port = 9000;
 
+app.use(cors())
 app.use(express.json())
 app.use('/api', route)
+
+app.use('/', function getDataController (req, res) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+})
 
 app.listen(port,() => {
     console.log(`\n * Server Started Successfully at ${port} * \n`);
