afd02d2b36a4cf853340a81baafe13af59f7e3ae
rohit001007 <rohit001007@gmail.com>
2021-01-16 20:26:40 +0530

    Added : Get operation Using Ajax

--- JavaScript/mainFile.js
+++ JavaScript/mainFile.js
@@ -1,7 +1,35 @@
-$(document).ready(function(){
-    $("button").click(function(){
-      $.get("demo_test.asp", function(data, status){
-        alert("Data: " + data + "\nStatus: " + status);
-      });
-    });
+$(document).ready(function () {
+
+  $.ajax({
+      type: 'GET',
+      url: "http://localhost:9000/api/employee/read",
+      dataType:"json",
+      success: function (data) {
+
+        console.log(data.data)
+        
+        let rowdata = ""
+
+        $.each(data.data, function (key, value) {
+
+          rowdata += "<tr>";
+
+          rowdata += "<td>" +value.firstName + "</td>";
+          rowdata += '<td>' +value.lastName + '</td>';
+          rowdata += '<td>' +value.eMail + '</td>';
+          rowdata += '<td>' +value.mobile + '</td>';
+          rowdata += '<td>' +value.companyName + '</td>';
+
+          rowdata += '<td><button class="button1"><img src="../Images/edit-message-svgrepo-com.svg"></button></td>';
+          rowdata += '<td><button class="button1"><img src="../Images/delete-contact-svgrepo-com.svg"></button></td>';             
+          
+          "</tr>";
+                                            
+        });
+        $('#table').append(rowdata);
+      },
+        error: function () {
+          alert('Error In Loading Data...!!!')
+        }
   });
+});
--- Server/server.js
+++ Server/server.js
@@ -10,10 +10,6 @@ app.use(cors())
 app.use(express.json()) 
 app.use('/api', route)
 
-app.use('/', function getDataController (req, res) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
-})
-
 app.listen(port,() => {
     console.log(`\n * Server Started Successfully at ${port} * \n`);
     require('../dbManagement/dbManagement')
